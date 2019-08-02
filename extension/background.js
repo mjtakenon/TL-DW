407b4aa3f696bbf87146c27e40b49bb124d8c919
mjtakenon <cs15503@s.inf.shizuoka.ac.jp>
2019-08-02 16:29:00 +0900

    APIキーをファイルから読み込むよう変更、Yahoo!API利用の実装

--- extension/background.js
+++ extension/background.js
@@ -1,25 +1,63 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 
-// Called when the user clicks on the browser action.
+// ファイルからAppIdを取得
+async function getAppId() {
+  var filename = "yahooAppId.txt";
+  return new Promise(function(resolve) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', chrome.extension.getURL(filename), true)
+    xhr.send()
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+        resolve(xhr.responseText)
+      }
+    }
+  })
+}
 
+// Yahoo!APIにアクセスしてキーワードを取得
+async function getKeyword(appId,str) {
+  return new Promise(function(resolve) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid='+appId+'&sentence='+encodeURIComponent(str)+'&output=json', true);
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    xhr.send()
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+        resolve(xhr)
+      }
+    }
+  })
+}
+
+// タグを生成
+async function getTags(str){
+  // appId取得
+  appId = await getAppId()
+  // キーワード取得
+  res = await getKeyword(appId,str)
+  console.log(JSON.parse(res.responseText))
+  // タグを表示
+  chrome.tabs.executeScript({
+    file: "showTags.js",
+  })
+}
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log("chrome.browserAction.onClicked")
-  chrome.tabs.executeScript({
-    file: "showTags.js"
-  })
+  str = "";
+  getTags(str)
+  
+  return;
 })
 
 // バグあり 既存のタグを消してしまう
 // このコードがない場合ページ遷移をしても追加されたタグが消えない
-chrome.tabs.onUpdated.addListener(function(tabid, info, tab){
-  console.log("chrome.tabs.onUpdated")
-  chrome.tabs.executeScript({
-    file: "clearTags.js"
-  })
-  // info.url
-  // tab.url
-})
+// chrome.tabs.onUpdated.addListener(function(tabid, info, tab){
+//   console.log("chrome.tabs.onUpdated")
+//   chrome.tabs.executeScript({
+//     file: "clearTags.js"
+//   })
+//   // info.url
+//   // tab.url
+// })
--- extension/showTags.js
+++ extension/showTags.js
@@ -1,12 +1,11 @@
 
 // タグを取ってくる処理を書く
-function getTags() {
-  return " " + "#名取さな #ヌォンタート";
-}
 
 console.log("show tags.");
 
 // 元のタグ要素を取得
-defaultHTML = document.getElementsByClassName("super-title style-scope ytd-video-primary-info-renderer")[0].innerHTML
+className = "super-title style-scope ytd-video-primary-info-renderer"
+defaultHTML = document.getElementsByClassName(className)[0].innerHTML
 // 元のタグ要素に取得してきたタグを連結して追加
-document.getElementsByClassName("super-title style-scope ytd-video-primary-info-renderer")[0].innerHTML = defaultHTML + getTags()
+document.getElementsByClassName(className)[0].innerHTML = defaultHTML + " " + str
+// chrome.extension.onRequest.addListener( onRequest ) ;
