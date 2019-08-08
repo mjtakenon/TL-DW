0a5f9a856d2787ee470faf58d6a28437acad08e9
Sanosuke Kato <plus.wisteria38@gmail.com>
2019-08-08 19:16:35 +0900

    [Add] Implement clear tags.

--- extension/clearTags.js
+++ extension/clearTags.js
@@ -1,4 +1,4 @@
-// console.log(document.getElementsByClassName("super-title style-scope ytd-video-primary-info-renderer")[0].innerHTML)
-// document.getElementsByClassName("super-title style-scope ytd-video-primary-info-renderer")[0].innerHTML = ""
-
-// document.getElementsByClassName("super-title style-scope ytd-video-primary-info-renderer")[0].innerHTML = localStorage.getItem("defaultInnerHTML")
+tags = document.querySelectorAll('.tl-dw-tag');
+for (let i = 0; i < tags.length; i++) {
+  tags[i].remove();
+}
--- extension/showTags.js
+++ extension/showTags.js
@@ -7,6 +7,6 @@ className = "super-title style-scope ytd-video-primary-info-renderer";
 innerHTML = document.getElementsByClassName(className)[0].innerHTML;
 for (let key in res) {
   // 元のタグ要素に取得してきたタグを連結して追加
-  innerHTML += '<a class="yt-simple-endpoint style-scope yt-formatted-string">#' + key + '</a>';
+  innerHTML += '<a class="yt-simple-endpoint style-scope yt-formatted-string tl-dw-tag">#' + key + '</a>';
 }
 document.getElementsByClassName(className)[0].innerHTML = innerHTML;
