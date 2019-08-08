10001801782811d7d82a999c690c32bb5078e176
Sanosuke Kato <plus.wisteria38@gmail.com>
2019-08-08 18:51:28 +0900

    [Fix] Views of tags.

--- extension/showTags.js
+++ extension/showTags.js
@@ -2,11 +2,11 @@
 console.log("showTags")
 
 // 元のタグ要素を取得
-className = "super-title style-scope ytd-video-primary-info-renderer"
+className = "super-title style-scope ytd-video-primary-info-renderer";
 
-for (var key in res) {
-  defaultHTML = document.getElementsByClassName(className)[0].innerHTML
+innerHTML = document.getElementsByClassName(className)[0].innerHTML;
+for (let key in res) {
   // 元のタグ要素に取得してきたタグを連結して追加
-  document.getElementsByClassName(className)[0].innerHTML = defaultHTML + " #" + key //str
+  innerHTML += '<a class="yt-simple-endpoint style-scope yt-formatted-string">#' + key + '</a>';
 }
-
+document.getElementsByClassName(className)[0].innerHTML = innerHTML;
