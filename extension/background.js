8b605034b87611d78cf491e314a22306dd862164
mjtakenon <takenon777@gmail.com>
2019-08-10 04:20:52 +0900

    wordcloud生成

--- extension/background.js
+++ extension/background.js
@@ -270,16 +270,21 @@ async function analizeString(str) {
   }
   merge_count.sort((a, b) => b[1] - a[1]);
   console.log(merge_count)
-  let filterd_merge_count = merge_count.filter(el => {
+  // 20以上のスコアのものだけ利用
+    let filterd_merge_count = merge_count.filter(el => {
     return el[1] >= 20;
   })
+  // 最大で10語まで返す
+  if (filterd_merge_count.length >= 10) {
+    filterd_merge_count = filterd_merge_count.slice(0, 10)
+  }
   return filterd_merge_count
 }
 
 // タグを生成
 async function showTags(tab, tagList){
-  // 以前生成されたタグがあったら消しとく
-  chrome.tabs.sendMessage(tab.id, "clearTags");
+  // 以前生成されたタグがあったら消しとく(うまくいかない)
+  // chrome.tabs.sendMessage(tab.id, "clearTags");
   chrome.tabs.sendMessage(tab.id, ["showTags",await getCurrentURL(),tagList]);
   // // chrome.tabs.executeScript({
   // //   file: "clearTags.js"
@@ -746,17 +751,11 @@ async function main(tab) {
 // })
 function onClickedAnalyze() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log("onclickedAnalyze")
+    console.log(tabs)
     main(tabs[0])
   })
 }
-
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    console.log(request)
-    // main(tab)
-    return
-  }
-)
 
 // 別動画に遷移したときに，表示していたタグを削除
 chrome.tabs.onUpdated.addListener((tabid, info, tab) => {
@@ -765,6 +764,68 @@ chrome.tabs.onUpdated.addListener((tabid, info, tab) => {
   checkSamePageAndLoadTags(tab, info)
 });
 
+
+// https://qiita.com/january108/items/5388799531c1ace8324e
+function clickedWordCloud() {
+  var DATA_FILE_PATH = './tmp.json'; // 読み込みデータファイル
+  var TARGET_ELEMENT_ID = '#cloud'; // 描画先
+  let tagList = JSON.parse(localStorage.getItem("prevTagList"))
+  console.log(tagList)
+  let data = "["
+  for(itr of Object.keys(tagList)) {
+    data += "{" + '"word":"' + tagList[itr][0] +'","count":'+ tagList[itr][1] +'}'
+    if (itr != tagList.length-1) {
+      data += ','
+    }
+  }
+  data += "]"
+  data = JSON.parse(data)
+  // d3.json(DATA_FILE_PATH).then(function(data) { // v5
+  var h = 490;
+  var w = 600;
+
+  var random = d3.randomIrwinHall(2);
+  var countMax = d3.max(data, function(d){ return d.count} );
+  var sizeScale = d3.scaleLinear().domain([0, countMax]).range([10, 100])
+
+  var words = data.map(function(d) {
+    return {
+    text: d.word,
+    size: sizeScale(d.count) //頻出カウントを文字サイズに反映
+    };
+  });
+
+  d3.layout.cloud().size([w, h])
+    .words(words)
+    .rotate(function() { return (~~(Math.random() * 6) - 3) * 30; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw) //描画関数の読み込み
+    .start();
+
+  // wordcloud 描画
+  function draw(words) {
+    d3.select(TARGET_ELEMENT_ID)
+      .append("svg")
+        .attr("class", "ui fluid image") // style using semantic ui
+        .attr("viewBox", "0 0 " + w + " " + h )  // ViewBox : x, y, width, height
+        .attr("width", "100%")    // 表示サイズの設定
+        .attr("height", "100%")   // 表示サイズの設定
+      .append("g")
+        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return d3.schemeCategory10[i % 10]; })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
+}
 document.getElementById('analyze').addEventListener('click', onClickedAnalyze)
 document.getElementById('word-cloud').addEventListener('click', clickedWordCloud)
-document.getElementById('log').addEventListener('click', clickedLog)
+document.getElementById('info').addEventListener('click', clickedInfo)
